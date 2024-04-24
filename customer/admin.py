580b42f3d2c67bf077b42aa1204823296a9313c0
phanthang07 <phanvanthangphan07@gmail.com>
2024-04-24 09:59:03 +0700

    24-04

--- customer/admin.py
+++ customer/admin.py
@@ -3,7 +3,7 @@ from .models import Customer
 
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'locations', 'contact']
+    list_display = ['customercode', 'customertype', 'name', 'locations',]
     list_filter = ['name']
     search_fields = ['name']
 admin.site.register(Customer, CustomerAdmin)
--- customer/models.py
+++ customer/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
+    customercode = models.CharField(max_length = 255)
+    customertype = models.CharField(max_length = 255)
     name = models.CharField(max_length = 255)
-    locations = models.CharField(max_length = 255)
-    contact = models.TextField()
+    locations = models.TextField()
+    # contact = models.TextField()
     
     def __str__(self) -> str:
         return super().__str__()
